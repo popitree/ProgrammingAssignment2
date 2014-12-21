eefa978b2fd93a2942427773ffb6d60e002e98d3
popitree <indranilnanda@gmail.com>
2014-12-20 20:20:31 -0700

    Assignment Submit -popitree

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First Function creates a LIST that will contain getter setter for the Input MATRIX and Output InverseMatrix
+## Second Function will take the LIST from First function as input. Will try to get inverse of matrix from cache first.
+##if not found then will compute
 
-## Write a short comment describing this function
+## Will take matrix as argument. Return a list containing getter setter methods
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix  <- function(matrixX = matrix()) {
 
+  	inverseOfMatrix <- NULL		#computedValue
+
+  	set<-function(matrixY){
+  		matrixX <<- matrixY
+  		inverseOfMatrix <<- NULL
+	}
+
+	get<-function() matrixX 
+
+	setInverseMatrix <- function(solve) inverseOfMatrix <<- solve
+
+	getInverseMatrix<-function() inverseOfMatrix 
+
+	list(	set=set, 
+		get=get,
+		setInverseMatrix = setInverseMatrix,
+   		getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. It will try to get from cache first
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve  <- function(x,...){
+
+	inverseOfMatrix  <- x$getInverseMatrix()
+	if(!is.null(inverseOfMatrix)){
+		message("getting cached data")
+            return(inverseOfMatrix)
+	}
+	
+	data <- x$get()
+	inverseOfMatrix <- solve(data,...)
+	x$setInverseMatrix(inverseOfMatrix)
+	
 }
